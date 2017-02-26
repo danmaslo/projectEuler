8e315612caa21ef6a1e40b0b98c850150c28dddb
Daniel Máslo <hi@danielmaslo.com>
2017-02-26 08:48:13 +0100

    problem 18

---
+++ src/problems/E018.java
@@ -0,0 +1,55 @@
+package problems;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/** 
+ * @link http://projecteuler.net/problem=18
+ * @author hi@danielmaslo.com
+ */
+public class E018 {
+
+    public static long solution() {
+
+		int[][] matrix = new int[15][15];
+		String filename = "./src/input/E018.dat";
+
+		// convert file to matrix
+		try {
+			matrix = E018.importTriangleMatrix(15, filename);
+		} catch (FileNotFoundException e) {
+			System.out.println("There is no file " + filename);
+		}
+
+		// solving
+        int rows = matrix[0].length;
+
+		for (int i = rows - 2; i >= 0; i--) {
+			for (int j = 0; j < matrix.length - 1; j++) {
+				matrix[i][j] += Math.max(matrix[i+1][j], matrix[i+1][j+1]);
+			}
+		}
+
+		return(matrix[0][0]);
+	}
+
+	public static int[][] importTriangleMatrix(int size, String filename) throws FileNotFoundException {
+		Scanner cin = new Scanner(new File(filename));
+		int [][] result = new int[size][size];
+
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (j > i) {
+					result[i][j] = 0;
+				}
+				else {
+					result[i][j] = cin.nextInt();
+				}
+			}
+		}
+
+		return(result);
+	}
+	
+}
